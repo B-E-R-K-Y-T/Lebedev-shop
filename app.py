96c5af9e7c04c5aec5637400333898a64c421e74
Alex5405 <72857965+Alex5405@users.noreply.github.com>
2023-03-07 18:11:52 +0300

    Исправление названий классов. Которые я косячно назвал. Сори парни)))

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from tools.database import create_db, session
 from settings.config import PATH_GET_PRODUCT_REQUEST, RequestMethod, PATH_CREATE_PRODUCT_REQUEST, \
     PATH_UPDATE_PRODUCT_REQUEST, PATH_DELETE_PRODUCT_REQUEST, PATH_CREATE_CART_REQUEST, PATH_GET_CART_REQUEST, \
     GET_CART_ANSWER, PATH_UPDATE_CART_REQUEST
-from models.products import Product
+from models.products import Products
 from models.carts import Cart
-from models.carts_products import CartProducts
+from models.carts_products import CartsProducts
 from tools.request_worker import ProductRequest
 
 app = Flask(__name__)
@@ -18,7 +18,7 @@ client = app.test_client()
 @app.route(PATH_GET_PRODUCT_REQUEST, methods=[RequestMethod.GET])
 def get_product(id_product):
     pr = ProductRequest()
-    product = Product.query.filter(Product.id == id_product).first()
+    product = Products.query.filter(Products.id == id_product).first()
     serialized: dict = {}
 
     for key in pr.get_product_answer:
@@ -31,7 +31,7 @@ def get_product(id_product):
 def create_product():
     fields: dict = request.json
     fields['id'] = random.randint(0, 100_000)
-    new_obj = Product(**fields)
+    new_obj = Products(**fields)
     session.add(new_obj)
     session.commit()
     serialized = {'id': new_obj.id}
@@ -40,7 +40,7 @@ def create_product():
 
 @app.route(PATH_UPDATE_PRODUCT_REQUEST, methods=[RequestMethod.PUT])
 def update_product(id_product):
-    product = Product.query.filter(Product.id == id_product).first()
+    product = Products.query.filter(Products.id == id_product).first()
     params = request.json
     if product:
         for k, v in params.items():
@@ -55,7 +55,7 @@ def update_product(id_product):
 
 @app.route(PATH_DELETE_PRODUCT_REQUEST, methods=[RequestMethod.DELETE])
 def delete_product(id_product):
-    product = Product.query.filter(Product.id == id_product).first()
+    product = Products.query.filter(Products.id == id_product).first()
     if product:
         session.delete(product)
         session.commit()
